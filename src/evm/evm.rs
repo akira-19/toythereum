8dfb6f457a852f87d84a483845142b079de4b9c0
Akira T <akira.tsuruoka.19@gmail.com>
2024-11-08 11:03:42 +0900

    remove unnecessary codes

--- src/evm/evm.rs
+++ src/evm/evm.rs
@@ -167,7 +167,7 @@ impl EVM {
 
                 0x35 => self.op_calldataload(),
 
-                0x39 => self.op_codecopy(code_storage),
+                0x39 => self.op_codecopy(),
 
                 0x51 => self.op_mload(),
                 0x52 => self.op_mstore(),
@@ -277,7 +277,7 @@ impl EVM {
         self.consume_gas(U256::from(3));
     }
 
-    fn op_codecopy(&mut self, code_storage: &CodeStorage) {
+    fn op_codecopy(&mut self) {
         let dest_offset = self.stack.pop().as_u32() as usize;
         let offset = self.stack.pop().as_u32() as usize;
         let length = self.stack.pop().as_u32() as usize;
@@ -399,65 +399,5 @@ impl EVM {
 
         let return_value = &self.memory[offset..offset + length];
         self.returns = Some(ReturnValue::Revert(Vec::from(return_value)));
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-
-    use crate::state;
-
-    use super::*;
-
-    #[test]
-    fn test_addition() {
-        let code = vec![
-            0x60, 0x01, // PUSH1
-            0x60, 0x02, // PUSH1
-            0x01, // ADD
-            0x60, 0x00, // PUSH1
-            0x52, // MSTORE
-            0x60, 0x20, // PUSH1
-            0x60, 0x00, // PUSH1
-            0xF3, // RETURN
-        ];
-        // [74] PUSH1 0x02
-        // [75] PUSH1 0x01
-        // [76] ADD
-        // [77] DUP1
-        // [78] PUSH1 0x40
-        // [79] MSTORE
-        // [80] PUSH1 0x20
-        // [81] PUSH1 0x40
-        // [82] RETURN
-
-        // PUSH1 0x01    // スタックに1をプッシュ
-        // PUSH1 0x02    // スタックに2をプッシュ
-        // ADD           // スタックの値を足して結果をスタックにプッシュ
-        // PUSH1 0x00    // メモリの開始位置（オフセット0）
-        // MSTORE        // メモリにスタックの値を保存
-        // PUSH1 0x20    // 返すデータのサイズ（32バイト）
-        // PUSH1 0x00    // メモリの開始位置（オフセット0）
-        // RETURN
-        // let contract_address = Address::new("0x1234567890123456789012345678901234567890");
-        // let mut code_storage = CodeStorage::new();
-        // code_storage.insert_code(contract_address.clone(), code.clone());
-        // let mut evm = EVM::new(
-        //     contract_address,
-        //     Address::new("0x1234567890123456789012345678901234567890"),
-        //     U256::zero(),
-        //     Vec::new(),
-        //     U256::zero(),
-        //     U256::from(1000),
-        //     WorldState::new(),
-        //     code_storage,
-        // );
-
-        // let mut storage = state::StorageTrie(HashMap::new());
-
-        // evm.run(&mut storage);
-
-        // assert_eq!(evm.returns, Some(ReturnValue::Return([3u8])));
     }
 }
